d852a5c0715616d96fd99688de53d5a3c403c4f9
shmsacr <shmsacr@gmail.com>
2023-08-16 15:01:43 +0300

    Initial commit

---
+++ src/main/java/com/example/Flight_Search_API/FlightSearchApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Flight_Search_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Flight_Search_API/FlightSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Flight_Search_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
